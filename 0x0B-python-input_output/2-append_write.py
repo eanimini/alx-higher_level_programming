8e5eeec9c19c3241ffd4476299f066a66c93452f
eanimini <minio.afework@gmail.com>
2021-12-21 12:47:09 -0800

    0x0B. Python - Input/Output correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 function that appends a string at the end of a text file (UTF8) and returns the number of characters added
 """
 
+
 def append_write(filename="", text=""):
     with open(filename, 'a') as f:
         return f.write(text)
